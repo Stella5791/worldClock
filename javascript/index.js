258b40559c55cd3936637fee0e62a4f44a7fdebf
Stella5791 <142344371+Stella5791@users.noreply.github.com>
2023-11-24 18:34:39 -0300

    added JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
+function updateTime() {
+  let cordobaElement = document.querySelector("#cordoba");
+  let cordobaDateElement = cordobaElement.querySelector(" .date");
+  let cordobaTimeElement = cordobaElement.querySelector(" .time");
+  let cordobaTime = moment().tz("America/Argentina/Cordoba");
+  cordobaDateElement.innerHTML = cordobaTime.format("MMMM Do YYYY");
+  cordobaTimeElement.innerHTML = cordobaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(" .date");
+  let parisTimeElement = parisElement.querySelector(" .time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
