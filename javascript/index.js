c468b856798f074f56b0520c37a7e17f7ea349ee
Stella5791 <142344371+Stella5791@users.noreply.github.com>
2023-11-27 13:05:31 -0300

    added change cities, homepage link and new cities in html

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,38 @@
 function updateTime() {
-  let cordobaElement = document.querySelector("#cordoba");
-  if (cordobaElement) {
-    let cordobaDateElement = cordobaElement.querySelector(" .date");
-    let cordobaTimeElement = cordobaElement.querySelector(" .time");
-    let cordobaTime = moment().tz("America/Argentina/Cordoba");
+  //Mexico City
+  let mexicoCityElement = document.querySelector("#mexico-city");
+  if (mexicoCityElement) {
+    let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
+    let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
+    let mexicoCityTime = moment().tz("America/Mexico_City");
 
-    cordobaDateElement.innerHTML = cordobaTime.format("MMMM Do YYYY");
-    cordobaTimeElement.innerHTML = cordobaTime.format(
+    mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do YYYY");
+    mexicoCityTimeElement.innerHTML = mexicoCityTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
-  let parisElement = document.querySelector("#paris");
-  if (parisElement) {
-    let parisDateElement = parisElement.querySelector(" .date");
-    let parisTimeElement = parisElement.querySelector(" .time");
-    let parisTime = moment().tz("Europe/Paris");
-    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-    parisTimeElement.innerHTML = parisTime.format(
+  // Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Pacific/Sydney");
+
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  //Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
@@ -39,7 +55,9 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
-  `;
+      `;
+  let linkHome = document.querySelector("#link-home");
+  linkHome.innerHTML = `<a href="/">Return to Homepage</a>`;
 }
 
 updateTime();
